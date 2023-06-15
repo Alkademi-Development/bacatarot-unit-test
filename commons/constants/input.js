e7ce3b7734fb91aa46d8481c53bb8b64aefa63fb
AdnanErlansyah <adnanerlansyah403@gmail.com>
2023-06-15 08:32:39 +0700

    (+) feature remove file failed screenshoot

--- commons/constants/input.js
+++ commons/constants/input.js
@@ -2,10 +2,12 @@ import clc from "cli-color";
 
 const ALL_TEXT_INPUT = {
     questionInputReportFile: `2. Apakah anda ingin membuat file laporan / report testnya terpisah ?, \n jika iya masukkan nama report (formatnya ${clc.yellow("'tester'")} atau ${clc.yellow("'tester-file'")} atau jika anda ingin menaruh nya di dalam folder bisa seperti ini ${clc.yellow("'folder/tester-file'")} ) : `,
-    
 };
 
+const TEXT_CONFIRM = ['y', 't'];
+
 export {
-    ALL_TEXT_INPUT
+    ALL_TEXT_INPUT,
+    TEXT_CONFIRM
 }
 
